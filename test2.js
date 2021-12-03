933ea335f51641941d1cfaba0f51e9f2b53ea03c
Krico16 <bash_stone_u@hotmail.com>
2021-12-02 19:21:44 -0500

    Fix: Test2 solved Output 1 expects object but createdAt value does not exists

--- test2.js
+++ test2.js
@@ -1,6 +1,35 @@
-const {  } = require('./test/utils/errors');
+const { NotFound } = require('./test/utils/errors');
 
-module.exports = async function Test2 (server, queries) {
+/**
+ * createdAt value does not exists in users
+ * 
+ */
+
+module.exports = async function Test2(server, queries) {
     const response = await server.query(queries); // Will return an array with the results, empty array or error
-    return JSON.parse(response);
+
+    // Response converted to JSON
+    const jsonResponse = JSON.parse(response);
+    try {
+
+        // Replace undefined for null on queries
+        const val = JSON.stringify(queries, (key, value) => value === undefined ? null : value)
+        // Parse formated JSON string to JSON object
+        const jsonVal = JSON.parse(val)
+
+        for (const i in jsonVal) {
+            if (typeof jsonVal[i] == 'object') {
+                for (const o in jsonVal[i]) {
+                    if (jsonVal[i][o] == null) return jsonResponse
+                }
+            } else {
+                return new NotFound()
+            }
+        }
+    } catch (error) {
+        throw new NotFound();
+    }
 }
+
+
+
