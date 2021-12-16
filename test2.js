b83effb39938e8b05c7525ef101efbbab8b820a4
Krico16 <bash_stone_u@hotmail.com>
2021-12-16 02:42:25 -0500

    Fixed and optimized code Thanks to @matiaslopezd

--- test2.js
+++ test2.js
@@ -1,15 +1,13 @@
-const { NotFound } = require('./test/utils/errors');
+const { NotFound, ServerError } = require('./test/utils/errors');
 
 module.exports = async function Test2(server, queries) {
     const response = await server.query(queries); // Will return an array with the results, empty array or error
-
-    const jsonResp = JSON.parse(response);
-    const jsonLenght = Object.keys(jsonResp).length;
-
-    if(jsonLenght <= 0){
-        return new NotFound();
-    }else{
-        return jsonResp;
+    try {
+        const jsonResp = JSON.parse(response);
+        const jsonLenght = Object.keys(jsonResp).length;
+        return (jsonLenght <= 0) ? jsonResp : new NotFound();
+    } catch (error) {
+        return new ServerError();
     }
 }
 
